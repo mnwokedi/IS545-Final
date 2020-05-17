984f80c26841c40c05cc9d30f9dda48dcf37d3fd
mnwokedi <60416811+mnwokedi@users.noreply.github.com>
2020-05-16 20:34:16 -0700

    2nd

---
+++ finaljs.js
@@ -0,0 +1,25 @@
+var start = document.getElementById("number1");
+var end = document.getElementById("number2");
+
+var button = document.querySelector(".submit-button");
+var form = document.querySelector(".form");
+function checkInput() {
+  if (start.value.trim() !== "" && end.value.trim() !== "") {
+    button.style.backgroundColor = "#fa923f";
+    button.disabled = false;
+  } else {
+    button.style.backgroundColor = "#979695";
+    button.disabled = true;
+  }
+}
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  for(i = 20; i <= 100; i++)
+{
+    i = e;
+    if( e ÷ 2 == 0)
+    {
+        print(e)
+    }
+}
